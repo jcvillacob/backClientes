c44b07eb4e2e33ba27fc36e682a52895e2af192d
jvillacob <jvillacob@coorditanques.com>
2024-11-06 17:08:56 -0500

    feature: swagger docs

--- src/modules/cloudFleet/routes/cloudFleetRoutes.js
+++ src/modules/cloudFleet/routes/cloudFleetRoutes.js
@@ -3,7 +3,6 @@ const cloudFleetController = require('../controllers/cloudFleetController');
 const router = express.Router();
 
 router.get('/vehicles', cloudFleetController.getAllVehicles);
-// New route for getting work orders
 router.get('/work-orders', cloudFleetController.getWorkOrders);
 router.get('/work-order', cloudFleetController.getWorkOrder);
 
